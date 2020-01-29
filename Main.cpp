65ada7e21bb191868e736b540bd10bf349dc97c6
U-BSD\411036 <411036@SHS-10DEC4F83.beaverton.k12.or.us>
2020-01-29 09:10:55 -0800

    file read

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,7 @@ Heap demo main class
 
 #include "Node.h"
 #include <iostream>
+#include <fstream>
 #include <cstring>
 
 using namespace std;
@@ -13,19 +14,31 @@ using namespace std;
 int main() {
   cout << "Welcome to Heap demo. Type \"m\" for manual input or \"f\" for file input" << endl;
   char input;
-  
+  char* line;
   bool accept = false;
   while (!accept) {
     cin >> input;
     cin.clear();
     cin.ignore(999, '\n');
     if (input == 'f') {
+      cout << "Input file name" << endl;
+      char* fileName; cin.get(fileName, 99); cin.clear(); cin.ignore(999, '\n');
+      cout << "Reading from \"" << fileName << "\"" << endl;
+      ifstream fileStream (fileName);
+      if (fileStream.is_open()) {
+	fileStream.getline(line, 99);
+      }
+      else {
+	cout << "Could not read from file" << endl;
+	return 0;
+      }
+      fileStream.close();
       accept = true;
-      cout << "Please enter file name" << endl;
     }
     else if (input == 'm') {
       accept = true;
       cout << "Please enter a space separated list of integers" << endl;
+      cin.get(line, 999); cin.clear(); cin.ignore(999, '\n');
     }
     else {
       cout << "Please enter either \"m\" for manual input or \"f\" for file input" << endl;
