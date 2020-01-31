1497031f668a0fd5a2164be2cc4e2f1986eacd03
U-DESKTOP-QBU2FKV\Huanhuan <411036@bsd48.org>
2020-01-30 18:46:35 -0800

    Minor comments

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,6 @@ Author: Peter Jin
 Date: 1/11/20
 Heap demo main class
 */
-
 #include "Node.h"
 #include <iostream>
 #include <fstream>
@@ -11,37 +10,42 @@ Heap demo main class
 
 using namespace std;
 
+
+
 int main() {
   cout << "Welcome to Heap demo. Type \"m\" for manual input or \"f\" for file input" << endl;
   char input;
   char* line;
   bool accept = false;
+  //Reading in input now from console or from file
   while (!accept) {
     cin >> input;
     cin.clear();
     cin.ignore(999, '\n');
-    if (input == 'f') {
+    if (input == 'f') {                             //File input
       cout << "Input file name" << endl;
-      char* fileName; cin.get(fileName, 99); cin.clear(); cin.ignore(999, '\n');
+      char* fileName; cin.get(fileName, 99);
+      cin.clear(); cin.ignore(999, '\n');
       cout << "Reading from \"" << fileName << "\"" << endl;
-      ifstream fileStream (fileName);
+      ifstream fileStream (fileName);               //Get ifstream of file
       if (fileStream.is_open()) {
-	fileStream.getline(line, 99);
+	fileStream.getline(line, 99);               //Read line from File
       }
-      else {
+      else {                                        //File is not readable
 	cout << "Could not read from file" << endl;
 	return 0;
       }
       fileStream.close();
       accept = true;
-    }
-    else if (input == 'm') {
+    }                                               //Console Input
+    else if (input == 'm') {    
       accept = true;
       cout << "Please enter a space separated list of integers" << endl;
       cin.get(line, 999); cin.clear(); cin.ignore(999, '\n');
     }
-    else {
+    else {                                          //Input mode not accepted
       cout << "Please enter either \"m\" for manual input or \"f\" for file input" << endl;
     }
   }
+  
 }
