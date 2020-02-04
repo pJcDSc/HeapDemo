c6ad4f493e0d7efbed874c1b96dd563dabbb5e57
U-DESKTOP-QBU2FKV\Huanhuan <411036@bsd48.org>
2020-02-03 18:31:34 -0800

    I think it's done

--- Main.cpp
+++ Main.cpp
@@ -11,10 +11,12 @@ Heap demo main class
 
 using namespace std;
 
-void makeHeap(Node* &head, char*); //Make a new heap out of head (NULL) and char* (space separated list of integers)
+int makeHeap(Node* &head, char*); //Make a new heap out of head (NULL) and char* (space separated list of integers)
 void addNode(Node* &head, char*); //Converts char* given to int, then adds a new node to the heap, re-sort
 void reSort(Node* n); //Pass in most recently added node, moves to top based on heap insertion rules
-void display(Node*, int);
+void display(Node*, int); //Displays heap (horizontally) using infix trasversal
+int deleteNode(Node* &head, int); //Passes in binary int as path (1 is right, 0 is left); returns deleted value
+void heapify(Node* n); //Takes top node and moves it downward (used in delete)
 
 int main() {
   cout << "Welcome to Heap demo. Type \"m\" for manual input or \"f\" for file input" << endl;
@@ -55,17 +57,27 @@ int main() {
   Node* head = NULL;
   
   cout << "Creating Heap..." << endl;
-  makeHeap(head, line);
-  cout << endl << endl << endl;
+  int heapLen = makeHeap(head, line);               //Store length of heap (to get last node in o(logn) time) and create heap
   display(head, 0);
+  cout << endl;
+  cout << "Deleting heap..." << endl;
+  while (heapLen > 0) {
+    cout << deleteNode(head, heapLen--);
+    if (heapLen > 0) cout << ", ";
+  }
+  cout << endl;
 }
 
-void makeHeap(Node* &head, char* line) {
-  char* build = new char[strlen(line)](); //temp string to store individual words
+int makeHeap(Node* &head, char* line) {
+  char* build = new char[strlen(line)+1]();          //temp string to store individual words
+  int len = 0;
   int ind = 0;
   for (int i = 0; i < strlen(line); i++) {
     if (line[i] == ' ') { //Space found
-      if (strlen(build) > 0) addNode(head, build); //Don't add empty
+      if (strlen(build) > 0) {
+	addNode(head, build); //Don't add empty
+	len ++;
+      }
       build = new char[strlen(line)]();
       ind = 0;
     }
@@ -73,7 +85,11 @@ void makeHeap(Node* &head, char* line) {
       if (isdigit(line[i])) build[ind++] = line[i]; //Don't add non-digits
     }
   }
-  if (strlen(build) > 0) addNode(head, build);
+  if (strlen(build) > 0) {
+    addNode(head, build);
+    len ++;
+  }
+  return len;
 }
 
 void addNode(Node* &head, char* nc) {
@@ -96,14 +112,12 @@ void addNode(Node* &head, char* nc) {
       next -> setLeft(new Node(num));
       next -> getLeft() -> setParent(next);
       reSort(next -> getLeft());
-      //display(head, 0);
       break;
     }
     if (!next -> getRight()) {
       next -> setRight(new Node(num));
       next -> getRight() -> setParent(next);
       reSort(next -> getRight());
-      //display(head, 0);
       break;
     }
     q.push(next -> getLeft());
@@ -111,6 +125,7 @@ void addNode(Node* &head, char* nc) {
   }
 }
 
+//takes a node at(or near) the bottom and switches it with its parent if it is bigger
 void reSort(Node* n) {
   if (!n || !n -> getParent()) return;              //1 or empty node list
   if (n -> getParent() -> getValue() <
@@ -125,8 +140,72 @@ void reSort(Node* n) {
 
 //Display using sideways print based on infix navigation
 void display(Node* n, int d) {
+  if (!n) return;                                  //empty heap
   if (n -> getLeft()) display(n -> getLeft(), d+1);
-  for(int i = 0; i < d; i++) cout << "-  ";
+  for(int i = 0; i < d; i++) cout << "   ";
   cout << n -> getValue() << endl;
   if (n -> getRight()) display(n -> getRight(), d+1);
+}
+
+//Delete head, return it
+int deleteNode(Node* &head, int path) {
+  if (!head) {
+    return NULL;                                   //List is empty
+  }
+  Node* lastNode = head;
+  
+  int rev = 0;
+  int len = -1;                                    //We remove one digit at the end
+  while (path > 0) {                               //invert path (if path was 1100101 in binary, now is 1010011)
+    rev *= 2;
+    rev += path % 2;
+    path /= 2;
+    len++;
+  } 
+  path = rev / 2;                                  //Remove last digit of binary, because in original binary we disregard it
+  while (len-- > 0) {
+    if (path % 2 == 1) {                           //if odd go right
+      lastNode = lastNode -> getRight();
+    }
+    else {
+      lastNode = lastNode -> getLeft();
+    }
+    path /= 2;
+  }
+
+  if (lastNode == head) {                           //One node list
+    Node* temp = head;
+    int ret = head -> getValue();
+    head = NULL;
+    delete temp;
+    return ret;
+  }
+
+  int ret = head -> getValue();
+  head -> setValue(lastNode -> getValue());
+  if (lastNode -> getParent() -> getRight() == lastNode) {
+    lastNode -> getParent() -> setRight(NULL);
+  } else {
+    lastNode -> getParent() -> setLeft(NULL);
+  }
+  delete lastNode;
+  heapify(head);
+  return ret;
+}
+
+//Pass in head, switch it down
+void heapify(Node* head) {
+  Node* switchNode = NULL;
+  if (head -> getRight() && head -> getValue() < head -> getRight() -> getValue()) { //Check if any of the children are bigger, and if so which one is bigger
+    switchNode = head -> getRight();
+  }
+  if (head -> getLeft() && head -> getValue() < head -> getLeft() -> getValue()) {
+    if (!switchNode || switchNode -> getValue() < head -> getLeft() -> getValue()) switchNode = head -> getLeft(); //check which child is larger
+  }
+
+  if (!switchNode) return; //No children bigger
+  int temp = switchNode -> getValue();
+  switchNode -> setValue(head -> getValue());
+  head -> setValue(temp);
+  heapify(switchNode);
 }
